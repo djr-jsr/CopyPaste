1fe1738c71843d3cb83b1dcd469a6b6d3d010728
djr-jsr <djr.jsr@gmail.com>
2016-08-16 01:12:25 +0530

    Initial Commit

---
+++ content.js
@@ -0,0 +1,16 @@
+// content.js
+// console.log("Hi");
+// chrome.runtime.onMessage.addListener(
+  // function(request, sender, sendResponse) {
+  	// document.body.style.backgroundColor="green";
+  	document.addEventListener("paste", function (event) {
+	    event.stopPropagation();
+	}, true);
+	document.addEventListener("copy", function (event) {
+	    event.stopPropagation();
+	}, true);
+	document.addEventListener("cut", function (event) {
+	    event.stopPropagation();
+	}, true);
+  // }
+// );
