7a8bd96ac1a6609b2fe1adac95550c2981178873
ritesh-nitjsr <ritesh.nitjsr01@gmail.com>
2016-12-14 01:18:42 +0530

    Click to enable feature added.

--- background.js
+++ background.js
@@ -1,9 +1,20 @@
-// background.js
-// Called when the user clicks on the browser action.
+//This function is executed whenever the extension icon is clicked.
+//When the page loads, initially the icon is disabled.
+//And when the extension icon is clicked, Cut copy paste is enabled on that particular page.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+  var msg = {
+  	message: "enabled"
+  }
+  var titleObj = {
+    tabId: tab.id,
+    title: "Cut,copy,paste enabled."
+  }
+  var iconObj = {
+    tabId: tab.id,
+    path: "enabled.png"
+  }
+  chrome.browserAction.setTitle(titleObj);
+  chrome.browserAction.setIcon(iconObj);
+  //sends a message to the content script 
+  chrome.tabs.sendMessage(tab.id,msg);
 });
--- content.js
+++ content.js
@@ -1,19 +1,27 @@
-//for re-enabling 'paste'
-document.addEventListener("paste", function (event) {
-    event.stopPropagation();
-}, true);
+//gets activated on receiving a message from the background script
+chrome.runtime.onMessage.addListener(receiver);
 
-//for re-enabling 'copy'
-document.addEventListener("copy", function (event) {
-    event.stopPropagation();
-}, true);
+// Callback for when a message is received
+function receiver(request, sender, sendResponse) {
+   if (request.message === "enabled") {
+		//for re-enabling 'paste'
+		document.addEventListener("paste", function (event) {
+		    event.stopPropagation();
+		}, true);
 
-//for re-enabling 'cut'
-document.addEventListener("cut", function (event) {
-    event.stopPropagation();
-}, true);
+		//for re-enabling 'copy'
+		document.addEventListener("copy", function (event) {
+		    event.stopPropagation();
+		}, true);
 
-//for re-enabling 'right-clicks'
-document.addEventListener("contextmenu", function(event){
-    event.stopPropagation();
-}, true);
+		//for re-enabling 'cut'
+		document.addEventListener("cut", function (event) {
+		    event.stopPropagation();
+		}, true);
+
+		//for re-enabling 'right-clicks'
+		document.addEventListener("contextmenu", function(event){
+		    event.stopPropagation();
+		}, true);
+    }
+}
